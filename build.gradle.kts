9a0992f9b5548e7becbdb116133f342ebbd01519
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-29 21:49:34 +1100

    Implemented Dokka for javadoc generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,9 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     kotlin("plugin.serialization") version "2.0.20"
+    id("org.jetbrains.dokka") version "1.9.20"
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
